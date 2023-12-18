0b25487fd138dcb8408ec5c7c73ddfde718d630e
zero <1356888297@qq.com>
2023-12-18 15:52:36 +0800

    chore: 更新依赖

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-import type { AxiosRequestConfig, AxiosResponse } from 'axios'
+import type {AxiosError, AxiosRequestConfig, AxiosResponse} from 'axios'
 import axios from 'axios'
 import NProgress from 'nprogress'
 
@@ -46,27 +46,70 @@ service.interceptors.response.use(
   },
 )
 
-function request<T = any>(config: CustomAxiosRequestConfig): Promise<T> {
-  return new Promise((resolve, reject) => {
-    service
-      .request<BaseResponse<T>>(config)
-      .then((res: AxiosResponse) => resolve(res.data))
-      .catch((err: { message: string }) => reject(err))
-  })
+// 请求封装对象
+const request = {
+  /**
+   * 发送 GET 请求
+   *
+   * @param {string} url - 请求的 URL
+   * @param {object} data - 请求参数
+   * @param {string} requestBaseUrl - 请求的基础 URL
+   * @returns {Promise<T>} - Promise 对象，包含请求结果
+   */
+  get<T = any>(url: string, data?: any, requestBaseUrl?: string): Promise<T> {
+    return request.request('GET', url, { params: data }, requestBaseUrl)
+  },
+
+  /**
+   * 发送 POST 请求
+   *
+   * @param {string} url - 请求的 URL
+   * @param {object} data - 请求参数
+   * @param {string} requestBaseUrl - 请求的基础 URL
+   * @returns {Promise<T>} - Promise 对象，包含请求结果
+   */
+  post<T = any>(url: string, data?: any, requestBaseUrl?: string): Promise<T> {
+    return request.request('POST', url, { data }, requestBaseUrl)
+  },
+
+  /**
+   * 发送上传文件的请求
+   *
+   * @param {string} url - 请求的 URL
+   * @param {object} data - 请求参数
+   * @param {string} requestBaseUrl - 请求的基础 URL
+   * @returns {Promise<T>} - Promise 对象，包含请求结果
+   */
+  upload<T = any>(url: string, data?: any, requestBaseUrl?: string): Promise<T> {
+    const headers = { 'Content-Type': 'multipart/form-data' }
+    return request.request<T>('POST', url, { data, headers, requestBaseUrl })
+  },
+
+  /**
+   * 发送通用请求
+   *
+   * @param {string} method - 请求方法（GET、POST 等）
+   * @param {string} url - 请求的 URL
+   * @param {object} data - 请求参数
+   * @param {string} requestBaseUrl - 请求的基础 URL
+   * @returns {Promise<T>} - Promise 对象，包含请求结果
+   */
+  request<T = any>(
+    method = 'GET',
+    url: string,
+    data?: any,
+    requestBaseUrl?: string,
+  ): Promise<T> {
+    return new Promise((resolve, reject) => {
+      service({ method, url, ...data, requestBaseUrl })
+        .then((res) => {
+          resolve(res.data as T)
+        })
+        .catch((e: Error | AxiosError) => {
+          reject(e)
+        })
+    })
+  },
 }
-
-request.get = <T = any>(url: string, params?: object): Promise<T> =>
-  request({
-    method: 'get',
-    url,
-    params,
-  })
-
-request.post = <T = any>(url: string, params?: object): Promise<T> =>
-  request({
-    method: 'post',
-    url,
-    data: params,
-  })
 
 export default request
